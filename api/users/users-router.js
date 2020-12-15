70d95aa6f39833a31c3a5f337910fbea77db6c3a
Erick Gonzalez <ericksinbox@gmail.com>
2020-12-15 18:13:20 -0500

    mvp

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
-const Users = require("./users-model.js");
 
-router.get("/", (req, res) => {
-    Users.find()
+const Users = require("./users-model.js");
+const restricted = require("../auth/restricted-middleware.js");
+const checkRole = require('../auth/check-role-middleware.js');
+
+router.get("/", restricted, checkRole('admin'), (req, res) => {
+  Users.find()
     .then(users => {
-        res.json(users);
+      res.json(users);
     })
     .catch(err => res.send(err));
 });
