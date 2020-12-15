763804ed77c2a67b88dd7e29d921b0d6f2a54783
Erick Gonzalez <ericksinbox@gmail.com>
2020-12-15 17:29:46 -0500

    api up

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const helmet = require("helmet");
 const cors = require("cors");
 
 
-const authRouter = require("./auth/auth-router.js");
+// const authRouter = require("./auth/auth-router.js");
 const userRouter = require("./users/users-router.js");
 
 const server = express();
@@ -12,7 +12,7 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use("/api/auth", authRouter);
+// server.use("/api/auth", authRouter);
 server.use("/api/users", userRouter);
 
 server.get("/", (req, res) => {
